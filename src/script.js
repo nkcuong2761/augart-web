1af12ca023338d184b6db5433d60fe6cc4bf69eb
nkcuong2761 <nkcuong2761@gmail.com>
2022-03-26 23:13:16 -0400

    bruh

--- src/script.js
+++ src/script.js
@@ -1,6 +1,14 @@
+let lat = 40.95501595681849;
+let lng = -76.88195822931112;
 window.onload = () => {
-     let places = staticLoadPlaces();
-     renderPlaces(places);
+    let places = staticLoadPlaces();
+    navigator.geolocation.getCurrentPosition((position) => {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        console.log(lat, lng)
+    });
+    renderPlaces(places);
+     
 };
 
 function staticLoadPlaces() {
@@ -8,8 +16,24 @@ function staticLoadPlaces() {
         {
             name: 'Magnemite',
             location: {
-                lat: 40.9536539,
-                lng: -76.8832972
+                lat: 40.95501595681849,
+                lng: -76.88195822931112
+            },
+            source: "assets/magnemite/scene.gltf",
+            position: {
+                x: -7,
+                y: 0,
+                z: -5
+            },
+            rotation: {
+                x: 0,
+                y: 40,
+                z: 0
+            },
+            scale: {
+                x: 0.5,
+                y: 0.5,
+                z: 0.5
             }
         },
     ];
@@ -19,16 +43,19 @@ function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
 
     places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+        let location = place.location
+        let source = place.source;
+        let position = place.position;
+        let rotation = place.rotation;
+        let scale = place.scale
 
         let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${lng};`);
+        model.setAttribute('gltf-model', `${source}`);
+        model.setAttribute('rotation', `${rotation.x} ${rotation.y} ${rotation.z}`);
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
-        // model.setAttribute('position', '0 0 158')
+        model.setAttribute('scale', `${scale.x} ${scale.y} ${scale.z}`);
+        model.setAttribute('position', `${position.x} ${position.y} ${position.z}`)
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
